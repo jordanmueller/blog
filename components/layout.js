c496d16e7f25c81884fcfba4d2caccd04be2537b
Jordan Mueller <jordanmueller@gmail.com>
2023-03-29 23:21:37 -0400

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Jordan Mueller';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'Fun times on GitHub';
 
 export default function Layout({ children, home }) {
   return (
@@ -14,7 +14,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Blog for learning about technology"
         />
         <meta
           property="og:image"
@@ -30,7 +30,7 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src="/images/profile.jpg"
+              src="/blog/images/profile.jpg"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
@@ -43,7 +43,7 @@ export default function Layout({ children, home }) {
             <Link href="/">
               <Image
                 priority
-                src="/images/profile.jpg"
+                src="/blog/images/profile.jpg"
                 className={utilStyles.borderCircle}
                 height={108}
                 width={108}
