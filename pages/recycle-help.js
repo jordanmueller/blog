e2f2592f63a37f9afe25b1ecb65f8ba9946647ff
Jordan Mueller <jordanmueller@gmail.com>
2023-04-17 23:11:19 -0400

    Clean up recycle help
    Use State to update the results
    
    use Props passed into the Results element.

--- pages/recycle-help.js
+++ pages/recycle-help.js
@@ -1,25 +1,41 @@
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
+import {useState} from 'react';
 
-function readFile() {
-    const reader = new FileReader();
-    reader.onload = 
-    reader.addEventListener('load', () => loaded(reader))
-    reader.readAsDataURL(photo.files[0]);
-}
-
-async function loaded(reader) {
-    const response = await fetch('https://jordanmueller-plastics.hf.space/run/predict', {
-        method: "POST", body: JSON.stringify({ "data": [reader.result] }),
-        headers: { "Content-Type": "application/json" }
-    });
-    const json = await response.json();
-    const label = json['data'][0]['confidences'][0]['label'];
-    results.innerHTML = `<br/><p>Your Plastic</p><img src="${reader.result}" width="200"><br/><p>Is this number:</p><img src="/blog/images/resin_code_${label}.png" width="200">`
-}
 
 export default function RecycleHelp() {
+
+    const [image, setImage] = useState('')
+    const [plasticNumber, setPlasticNumber] = useState('1')
+
+    function readFile() {
+        const reader = new FileReader();
+        reader.onload = 
+        reader.addEventListener('load', () => loaded(reader))
+        reader.readAsDataURL(photo.files[0]);
+    }
+
+    async function loaded(reader) {
+        const response = await fetch('https://jordanmueller-plastics.hf.space/run/predict', {
+            method: "POST", body: JSON.stringify({ "data": [reader.result] }),
+            headers: { "Content-Type": "application/json" }
+        });
+        const json = await response.json();
+        const label = json['data'][0]['confidences'][0]['label'];
+        setImage(reader.result);
+        setPlasticNumber(label);
+    }
+
+    function Results({image, plasticNumber}) {
+        return (
+            <div className={utilStyles.results}>
+                <img src={image} width="200"></img>
+                <img src={"/blog/images/resin_code_" + plasticNumber + ".png"} width="200"></img>
+            </div>
+        );
+    }
+
     return (
       <Layout recycleHelp>
         <header>
@@ -31,7 +47,7 @@ export default function RecycleHelp() {
             </div>
         </label>
         <input id="photo" type="file" onInput={readFile} style={{display: 'none'}} />
-        <div id="results"></div>
+        { image.length > 0 && <Results image={image} plasticNumber={plasticNumber} />}
       </Layout>
     );
   }
